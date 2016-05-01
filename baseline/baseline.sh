fde45d9024ea0b38a6cdd907b458d401fb0a7c72
Afonso Menegola <afonsomenegola@gmail.com>
2016-05-01 12:53:12 -0300

    Finishin baseline.sh until AUC. Folder structure and AUC remaining.

--- baseline/baseline.sh
+++ baseline/baseline.sh
@@ -1,44 +1,88 @@
+
+# VARIABLES. Please, note that the following variables are used along the BossaNova pipeline. 
+# They are set with the values described at the original paper. If you want to change them the 
+# final results may be different than the reported ones. More details about each variable can 
+# be found at the 'help' of each subprogram.
+folds_number="1 2 3 4 5 6 7 8 9 10"
+
 # Usage
 show_help() {
-	echo "USAGE: baseline.sh -m [MATLAB] -d [DATASET] -e [EXPERIMENT]"
+	echo "USAGE: baseline.sh -d [DATASET] -e [EXPERIMENT] -m [MATLAB]"
 	echo "WHERE: "
-	echo "	[MATLAB]	: full path to the Matlab directory;"
-	echo "	[DATASET]	: full path to the dataset directory;"
-	echo "	[EXPERIMENT] 	: LM or LMplus or LMH, as detailed at the paper;"
+	echo "	[DATASET]	: full path to the dataset directory (according to this repository)"
+	echo "	[EXPERIMENT] 	: 'lm' or 'lmplus' or 'lmh', as detailed at the paper "
+	echo "	[MATLAB] 	: Matlab path, eg /usr/local/matlab/bin/matlab "
 	echo ""
 	echo "	ATTENTION! This script assumes that the dependencies are settled in the path."
 	echo "	-> See README file for the list of dependencies;"
 }
 
+# Check if all needed arguments were informed (not empty)
+check_arguments() {
+	if [ -z $dir_dataset ] || [ -z $experiment ] || [ -z $dir_matlab ] ;
+	 	then
+		show_help
+		exit 1
+	fi
+}
+
 # Input arguments
 show_arguments() {
 	echo "ARGUMENTS: "
-	echo "dir_matlab=$dir_matlab"
 	echo "dir_dataset=$dir_dataset"
 	echo "experiment=$experiment"
+	echo "dir_matlab=$dir_matlab"
 }
 
 # Setup: create directories as needed
 create_directories() {
-	mkdir -p code/results.gitnot
+	echo "$(tput setaf 2)Creating auxiliary directories...$(tput sgr 0)"
+	mkdir -p code/results
+	echo "$(tput setaf 2)Creating auxiliary directories...: DONE!$(tput sgr 0)"
 }
 
 skinScan() {
+	echo "$(tput setaf 2)Step 1. Applying skinScan algorithm...$(tput sgr 0)"
 	case "$experiment" in
 		lmh) $dir_matlab -nodisplay -nodesktop -nosplash -r "experiment='lmh';datasetFolder='../$dir_dataset';run code/run_matlab.m;exit" ;;
 		lmplus) $dir_matlab -nodisplay -nodesktop -nosplash -r "experiment='lmplus';datasetFolder='../$dir_dataset';run code/run_matlab.m;exit" ;;
 		lm) $dir_matlab -nodisplay -nodesktop -nosplash -r "experiment='lm';datasetFolder='../$dir_dataset';run code/run_matlab.m;exit" ;;
+		teste) $dir_matlab -nodisplay -nodesktop -nosplash -r "experiment='teste';datasetFolder='../$dir_dataset';run code/run_matlab.m;exit" ;;
+		#TODO: retirar teste do script
 	esac
+	echo "$(tput setaf 2)Step 1. Applying skinScan algorithm...: DONE! "
 }
 
-# Reset in case getopts has been used previously in the shell.
-OPTIND=1         
+# Classification step via LibSVM
+classification() {
+	echo "$(tput setaf 2)Step 2. Classification: this can take a long time... $(tput sgr 0)"
+	for i in $folds_number
+	do
+		python ../resources/easy_titans.py code/results/${experiment}/${experiment}_train_${i}.svm ${dir_highlevel}/${experiment}/${experiment}_test_${i}.svm
+	done
+	echo "$(tput setaf 2)Step 2. Classification: DONE! $(tput sgr 0)"
+}
 
-# If there is no argument, tells the user how to use this script. 
-if [ $# -lt 1 ]; then 
-	show_help 
-	exit 1 
-fi
+# Calculate AUC
+calculate_auc() {	
+	echo "$(tput setaf 2)Step 3. Calculating AUC... $(tput sgr 0)"
+	
+	echo "T O D O !"
+	
+	echo "$(tput setaf 2)Step 3. Calculating AUC...: DONE! $(tput sgr 0)"
+}
+#####################################################################################
+#																					#
+#									MAIN PROGRAM									#
+#																					#
+#####################################################################################
+
+# If there is no enough arguments, tells the user how to use this script
+OPTIND=1         
+#if [ $# -lt 14 ]; then 
+#	show_help 
+#	exit 1 
+#fi
 
 # Parse the arguments
 while getopts "d:l:m:h:e:" opt; do
@@ -55,6 +99,9 @@ done
 #shift $((OPTIND-1))
 #[ "$1" = "--" ] && shift
 
+# Before starting, check arguments
+check_arguments
+
 # Setup
 show_arguments
 create_directories
@@ -64,6 +111,10 @@ skinScan
 
 # Classification & results (AUCs)
 #classification
+classification
 #calculate_auc
+calculate_auc
 
-# End of bossanova script
+# End of skinScan script
+echo ""
+echo "$(tput setaf 2)F I N I S H E D! $(tput sgr 0)"
